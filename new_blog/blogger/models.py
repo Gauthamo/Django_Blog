1f8cc97d57e4a82edc84b44535fa407e2ae44a99
Gautham M <24886528+Gauthamo@users.noreply.github.com>
2024-05-07 12:46:02 +0530

    Delete new_blog directory

--- new_blog/blogger/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import datetime
-# Create your models here.
-class Blog(models.Model):
-
-    title = models.CharField(max_length=100)
-    body = models.CharField(max_length=10000)
-    dateOfArticle = models.DateTimeField(default=datetime.now)
